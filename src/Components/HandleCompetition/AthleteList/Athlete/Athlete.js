0db76d2fa41ac2be9428e47d8724a2aab9274ae7
Andres Riimets <getandressed@Andress-MacBook-Pro.local>
2021-01-17 01:09:55 +0100

    Fixed table issues

--- src/Components/HandleCompetition/AthleteList/Athlete/Athlete.js
+++ src/Components/HandleCompetition/AthleteList/Athlete/Athlete.js
@@ -18,34 +18,14 @@ const Athlete = ({name, snatch, cnj, age, coachName, weight, attempt, lift, regi
 		)
 	} else {
 		return (
-					<tr className="stripe-dark">
-		          <td headers="name" className="pa3">{name}</td>
-		          <td headers="age" className="pa3">{age}</td>
-		          <td headers="weight" onClick={() => editAthleteWeight(name)} className="pa3">{weight}</td>
-		          <td headers="snatch" className="pa3">{snatch}</td>
-		          <td headers="cnj" className="pa3">{cnj}</td>
-		          <td headers="coachname" className="pa3">{coachName}</td>
-		         </tr>
-			/*<div className="pa1 tc flex">
-				<div className="fl w-80 w-third-ns pa2">
-					<h4>{name}</h4>
-				</div>
-				<div className="fl w-80 w-third-ns pa2 bl">
-					<h4>{age}</h4>
-				</div>
-				<div onClick={() => editAthleteWeight(name)} className="fl w-80 w-third-ns pa2 bl">
-					<h4>{weight}</h4>
-				</div>
-				<div className="fl w-80 w-third-ns pa2 bl">
-					<h4>{snatch}</h4>
-				</div>
-				<div className="fl w-80 w-third-ns pa2 bl">
-					<h4>{cnj}</h4>
-				</div>
-				<div className="fl w-80 w-third-ns pa2 bl">
-					<h4>{coachName}</h4>
-				</div>
-			</div>*/
+			<tr className="stripe-dark">
+		      <td headers="name" className="tc pa3">{name}</td>
+		      <td headers="age" className="tc pa3">{age}</td>
+		      <td headers="weight" onClick={() => editAthleteWeight(name)} className="tc pa3">{weight}</td>
+		      <td headers="snatch" className="tc pa3">{snatch}</td>
+		      <td headers="cnj" className="tc pa3">{cnj}</td>
+		      <td headers="coachname" className="tc pa3">{coachName}</td>
+		   </tr>
 		)
 	}
 }
--- src/Components/HandleCompetition/AthleteList/AthleteList.js
+++ src/Components/HandleCompetition/AthleteList/AthleteList.js
@@ -5,16 +5,16 @@ const AthleteList = ({ registeredAthletes, inCompetitionAthletes, lift, name, ed
 	if (registeredAthletes) {
 		return (
 			<div className="pa4">
-				<div className="overflow-auto">
-			    	<table className="f6 w-100 mw8 center" cellspacing="0">
+				<div className="center overflow-auto">
+			    	<table className="f6 w-100 mw8" cellSpacing="0">
 				      <thead>
 				        <tr className="stripe-dark">
-				          <th id="name" className="fw6 tl pa3 bg-white">Name</th>
-				          <th id="age" className="fw6 tl pa3 bg-white">Age</th>
-				          <th id="weight" className="fw6 tl pa3 bg-white">Weight</th>
-				          <th id="snatch" className="fw6 tl pa3 bg-white">Snatch</th>
-				          <th id="cnj" className="fw6 tl pa3 bg-white">CNJ</th>
-				          <th id="coachname" className="fw6 tl pa3 bg-white">Coach Name</th>
+				          <th id="name" className="fw6 pa3 bg-white">Name</th>
+				          <th id="age" className="fw6 pa3 bg-white">Age</th>
+				          <th id="weight" className="fw6 pa3 bg-white">Weight</th>
+				          <th id="snatch" className="fw6 pa3 bg-white">Snatch</th>
+				          <th id="cnj" className="fw6 pa3 bg-white">CNJ</th>
+				          <th id="coachname" className="fw6 pa3 bg-white">Coach Name</th>
 				        </tr>
 				      </thead>
 				      <tbody className="lh-copy">
@@ -37,45 +37,6 @@ const AthleteList = ({ registeredAthletes, inCompetitionAthletes, lift, name, ed
 				      </tbody>
 				   </table>
 			   </div>
-				{/*<div className="tc flex bb">
-									<div className="fl w-80 w-third-ns pa2">
-										<h4>Name</h4>
-									</div>
-									<div className="fl w-80 w-third-ns pa2">
-										<h4>Age</h4>
-									</div>
-									<div className="fl w-80 w-third-ns pa2">
-										<h4>Weight</h4>
-									</div>
-									<div className="fl w-80 w-third-ns pa2">
-										<h4>Snatch</h4>
-									</div>
-									<div className="fl w-80 w-third-ns pa2">
-										<h4>Clean And Jerk</h4>
-									</div>
-									<div className="fl w-80 w-third-ns pa2">
-										<h4>Coach Name</h4>
-									</div>
-								</div>
-								*/}
-				{/*<div>
-									{
-										// registeredAthletes.map((athlete, i) => {
-										// 	return (
-										// 		<Athlete 
-										// 			key={i}
-										// 			name={athlete.name}
-										// 			age={athlete.age}
-										// 			snatch={athlete.snatch}
-										// 			cnj={athlete.cnj}
-										// 			coachName={athlete.coachname}
-										// 			weight={athlete.weight}
-										// 			editAthleteWeight={editAthleteWeight}
-										// 		/>
-										// 	)
-										// })
-									}
-								</div>*/}
 			</div>
 		)
 	} else {

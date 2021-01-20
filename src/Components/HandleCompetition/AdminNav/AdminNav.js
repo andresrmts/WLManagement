71f9e375903ec0b921231866eec366c29f125b93
Andres Riimets <getandressed@Andress-MacBook-Pro.local>
2021-01-20 19:12:52 +0100

    cosmetic changes

--- src/Components/HandleCompetition/AdminNav/AdminNav.js
+++ src/Components/HandleCompetition/AdminNav/AdminNav.js
@@ -4,31 +4,46 @@ const AdminNav = ({status, toggleStatus, compRoute, adminToggle, onRouteChange})
 	return (
 		<div>
 			<h1>You are currently working on COMPETITIONNAME</h1>
-			<nav style={{display: 'flex', justifyContent: 'center'}}>
 				{status === 'started' 
-					? <p 
-						onClick={() => toggleStatus('paused')}
-						className="f6 pa3 underline pointer red ba">PAUSE COMPETITION</p>
-					:	<p 
-						onClick={() => toggleStatus('started')}
-						className="f6 pa3 underline pointer red ba">START COMPETITION</p>
+					? 	<nav style={{display: 'flex', justifyContent: 'center'}}>
+							<p 
+							onClick={() => toggleStatus('paused')}
+							className="f6 pa3 underline pointer red ba">PAUSE COMPETITION</p>
+							<p 
+							onClick={() => compRoute('home')}
+							className="f6 pa3 underline pointer">Competition Home</p>
+						</nav>
+					:	(status === 'paused'
+							? 	<nav style={{display: 'flex', justifyContent: 'center'}}>
+									<p 
+									onClick={() => toggleStatus('started')}
+									className="f6 pa3 underline pointer red ba">START COMPETITION</p>
+									<p 
+									onClick={() => compRoute('home')}
+									className="f6 pa3 underline pointer">Competition Home</p>
+								</nav>
+							: 	<nav style={{display: 'flex', justifyContent: 'center'}}>
+									<p 
+									onClick={() => toggleStatus('started')}
+									className="f6 pa3 underline pointer red ba">START COMPETITION</p>
+									<p 
+									onClick={() => compRoute('home')}
+									className="f6 pa3 underline pointer">Pending registrations</p>
+									<p 
+									onClick={() => compRoute('registered')}
+									className="f6 pa3 underline pointer">Accepted Registrations</p>
+									<p 
+									onClick={() => compRoute('athletelist')}
+									className="f6 pa3 underline pointer">Competitor List</p>
+									<p 
+									onClick={() => {
+										onRouteChange('competitionselection')
+										adminToggle(false)
+										}} 
+									className="f6 pa3 underline pointer">Exit</p>
+								</nav>
+						)
 					}
-				<p 
-				onClick={() => compRoute('home')}
-				className="f6 pa3 underline pointer">Pending registrations</p>
-				<p 
-				onClick={() => compRoute('registered')}
-				className="f6 pa3 underline pointer">Accepted Registrations</p>
-				<p 
-				onClick={() => compRoute('athletelist')}
-				className="f6 pa3 underline pointer">Competitor List</p>
-				<p 
-					onClick={() => {
-						onRouteChange('competitionselection')
-						adminToggle(false)
-					}} 
-					className="f6 pa3 underline pointer">Exit</p>
-			</nav>
 		</div>
 	)
 }
